99a549a3c138a8ff21ac61911b01e668b61956a2
maztfajar <nyepetke1@gmail.com>
2026-05-02 08:31:48 +0700

    chore: remove legacy maintenance scripts, temporary backups, and duplicate project files

--- test_models.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from backend.core.model_manager import model_manager
-
-async def test():
-    await model_manager.startup()
-    print(model_manager.available_models)
-
-asyncio.run(test())
